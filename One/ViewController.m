ff9077597ffe25b7d3753d9123e9b58bcd61d4fa
Lion User <User@Lions-Mac.local>
2012-07-01 14:57:42 +0800

    add image smart.jpg

--- One/ViewController.m
+++ One/ViewController.m
@@ -10,29 +10,17 @@
 
 @interface ViewController ()
 
+@property (nonatomic, weak) UIImage *smartImage;
+
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-}
+@synthesize smartImage = _smartImage;
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-    } else {
-        return YES;
-    }
+    return YES;
 }
 
 @end

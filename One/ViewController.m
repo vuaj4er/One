3c7ffa9b60d9e614d73301d0d4ad805232cbdb88
Lion User <User@Lions-Mac.local>
2012-07-01 15:48:02 +0800

    try to add gesture

--- One/ViewController.m
+++ One/ViewController.m
@@ -10,14 +10,29 @@
 
 @interface ViewController ()
 
-@property (nonatomic, weak) UIImage *smartImage;
+@property (nonatomic, weak) UIImageView *smartImageView;
 
 @end
 
 @implementation ViewController
 
-@synthesize smartImage = _smartImage;
+@synthesize smartImageView = _smartImageView;
 
+- (void)setSmartImageView:(UIImageView *)smartImageView
+{
+    _smartImageView = smartImageView;
+    [self.smartImageView addGestureRecognizer:[[UIPanGestureRecognizer alloc] init]];
+}
+
+- (void)pan:(UIPanGestureRecognizer *)gesture withImageView:(UIImageView *)imageView
+{
+    if ((gesture.state == UIGestureRecognizerStateChanged) ||
+        (gesture.state == UIGestureRecognizerStateEnded)) {
+        CGPoint panPoint = [gesture translationInView:self.view];
+        
+    }
+}
+     
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return YES;
